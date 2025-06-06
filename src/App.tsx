9e4af45c8750d943410f3862aa360c0ed1809bc1
Dassana <dassana002@gmail.com>
2025-06-06 14:10:31 +0530

    install react-bootstrap and bootstrap dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <p>Hello world</p>
+      <NavBar/>
     </div>
   );
 }
