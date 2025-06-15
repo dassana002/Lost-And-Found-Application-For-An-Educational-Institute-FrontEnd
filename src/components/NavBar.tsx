079289cf36832429622034972c39a6165ac042bd
Dassana <dassana002@gmail.com>
2025-06-15 20:18:06 +0530

    Add dark-themed Navbar component with Home, Features, and Pricing links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,20 @@
-function NavBar(){
-    return(
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function NavBar() {
+    return (
         <div>
-            <p>Hello World</p>
+            <>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </>
         </div>
     );
 }
