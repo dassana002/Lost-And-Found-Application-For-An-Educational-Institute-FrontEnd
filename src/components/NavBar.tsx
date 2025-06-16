7cdbb22c920601630cfc74b72cfc506766c705a1
Dassana <dassana002@gmail.com>
2025-06-16 13:53:09 +0530

    add a IJSE logo in the Nav Bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,36 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import './NavBar.css';
+import ijselogo from '../images/images.jpeg';
 
 function NavBar() {
     return (
-        <div>
-            <>
-                <Navbar bg="dark" data-bs-theme="dark">
-                    <Container>
-                        <Navbar.Brand href="#home">Lost And Found Application </Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#items">ITEM</Nav.Link>
-                            <Nav.Link href="#requests">REQUEST</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </>
-        </div>
+        <Navbar className="navbar" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home" className="navbar-brand">
+                    Lost & Found Hub
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-nav ms-auto">
+                        <Nav.Link href="#items" className="nav-link">Items</Nav.Link>
+                        <Nav.Link href="#requests" className="nav-link">Requests</Nav.Link>
+                    </Nav>
+                    <div className="ijse-logo ms-3">
+                        <img
+                            src={ijselogo}
+                            alt="IJSE Logo"
+                            style={{ 
+                                width: '80px', 
+                                height: '80px', 
+                                objectFit: 'cover',
+                                boxShadow: 'var(--shadow-soft)'
+                            }}
+                            className="rounded-circle logo-img"
+                        />
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
