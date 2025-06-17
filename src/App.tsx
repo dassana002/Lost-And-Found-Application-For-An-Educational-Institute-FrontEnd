f88ab35f94c72ca5cd1b9ede23f13a48e56f68ca
dassana <dassana002@gmail.com>
2025-06-17 11:53:03 +0530

    Heading Add to Item table

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "/";
 import { Request } from './components/request/Request';
 import { NavBar } from './components/NavBar';
+import { Item } from './components/item/Item';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <Request/>
+      <Item/>
     </div>
   );
 }
--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,7 +1,52 @@
-export const Item = ()=> {
-    return(
+import Table from "react-bootstrap/esm/Table";
+
+export const Item = () => {
+
+    // Create a Heading Array
+    const tHeadings: string[] = [
+        "Item_id",
+        "Name",
+        "Description",
+        "Created_date",
+        "Location",
+        "Status"
+    ];
+
+    return (
         <div>
-            <p>Hello Item</p>
+            <div>
+                <h2>Item Table</h2>
+                <Table striped bordered hover>
+                    <thead>
+                        {/* Heading add to Table */}
+                        <tr>
+                            <th scope="col">#</th>
+                            {tHeadings.map((heading, index) => (
+                                <th key={index} scope="col">{heading}</th>
+                            ))};
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan={2}>Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 }
